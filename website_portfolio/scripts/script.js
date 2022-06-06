86bb18e8add63b49f0f12ced443d7c4ed9870919
Elvis Boves <elvisboves@Elviss-MacBook-Pro.local>
2022-06-06 10:40:39 -0400

    type: feat
    body: Add navegation for small devices.
    
    footer: #00

--- website_portfolio/scripts/script.js
+++ website_portfolio/scripts/script.js
@@ -1,5 +1,7 @@
 const sections = document.querySelectorAll("section");
 const nav_ul = document.getElementById("nav-ul-id");
+const btn_nav_el = document.querySelector(".btn-nav");
+const header = document.querySelector(".header");
 
 function gen_nav_list() {
   sections.forEach((section) => {
@@ -8,3 +10,33 @@ function gen_nav_list() {
 }
 
 gen_nav_list();
+
+// This code opens/closes the nav bar on smaller divices
+function open_nav() {
+  btn_nav_el.addEventListener("click", function () {
+    header.classList.toggle("nav-open");
+    console.log("I was clicked");
+  });
+}
+
+open_nav();
+
+function smooth_scrolling(anchor) {
+  document.querySelector(anchor.getAttribute("href")).scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+// This function select all the a tags and make a smooth scrolling motion
+// when you click on the section tag on the nav
+function section_click() {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      smooth_scrolling(anchor);
+      header.classList.remove("nav-open");
+    });
+  });
+}
+// calling the section_click function
+section_click();
